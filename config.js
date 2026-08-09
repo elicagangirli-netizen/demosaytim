ee9cad4938f906ae285e2edbaf16cd778e5c010e
elicagangirli-netizen <elicagangirli@gmail.com>
2026-08-09 21:16:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,78 @@
+const SITE_CONFIG = {
+    "site": {
+        "name": "Mugenni",
+        "title": "Yoxlama Sayti",
+        "description": "Musiqi və kliplər",
+        "about": "",
+        "logo": "images/demo2.png",
+        "url": "",
+        "views": "10M+",
+        "year": "2026"
+    },
+    "appearance": {
+        "banner": "images/demo2.png",
+        "background": "",
+        "color": "#9d45ff"
+    },
+    "social": {
+        "instagram": "",
+        "tiktok": "",
+        "youtube": "",
+        "spotify": "",
+        "facebook": "",
+        "telegram": "",
+        "whatsapp": "",
+        "email": "",
+        "phone": "",
+        "address": ""
+    },
+    "categories": [
+        "Azeri"
+    ],
+    "songs": [
+        {
+            "id": 1786301710481,
+            "name": "demo",
+            "title": "demo",
+            "artist": "demo",
+            "category": "Azeri",
+            "cover": "",
+            "audio": "music/demo.mp3",
+            "video": "",
+            "year": "2026"
+        },
+        {
+            "id": 1786301797125,
+            "name": "mahni",
+            "title": "mahni",
+            "artist": "demo",
+            "category": "Azeri",
+            "cover": "",
+            "audio": "music/aramam.mp3",
+            "video": "",
+            "year": "2026"
+        }
+    ],
+    "clips": [],
+    "links": [],
+    "artistName": "Mugenni",
+    "artistFullName": "Yoxlama Sayti",
+    "artistDescription": "Musiqi və kliplər",
+    "aboutText": "Musiqi və kliplər",
+    "artistImage": "images/demo2.png",
+    "stats": {
+        "songs": 2,
+        "videos": 0,
+        "views": "10M+",
+        "year": "2026"
+    },
+    "contact": {
+        "email": "",
+        "whatsapp": ""
+    },
+    "videos": [],
+    "gallery": [
+        "images/demo2.png",
+        "images/artist.png"
+    ]
+};
